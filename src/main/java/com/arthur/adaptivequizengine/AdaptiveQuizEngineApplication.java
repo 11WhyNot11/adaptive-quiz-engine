f51675798a3e5dff76f86efb2fa9a6837a688fd6
11WhyNot11 <arturrebar8@gmail.com>
2025-06-16 11:45:16 +0300

    initial commit – base setup working

---
+++ src/main/java/com/arthur/adaptivequizengine/AdaptiveQuizEngineApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.adaptivequizengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveQuizEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptiveQuizEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arthur/adaptivequizengine/AdaptiveQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.adaptivequizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptiveQuizEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
